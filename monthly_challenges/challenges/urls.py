bd9ace8bc7b47c346354357e93c7f3dbcd652422
Janotelo <janos.jantoc@gmail.com>
2023-08-08 18:56:22 +0800

    Done adding another view as a test.

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # imported views.py
 
 urlpatterns = [
-    path("january", views.index)  # called the function in the views.
+    path("january", views.index),  # called the function in the views.
+    path("february", views.february)
 ]  # List for URLS that is needed to be supported.
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This works!")
+
+def february(request):
+    return HttpResponse("This is february!")
