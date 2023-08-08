33085103c1b9f5d5aaceaf8cb16ca8a7f8689390
Janotelo <janos.jantoc@gmail.com>
2023-08-08 16:48:40 +0800

    Added the urls of challenges app.

--- monthly_challenges/monthly_challenges/urls.py
+++ monthly_challenges/monthly_challenges/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# added include inorder to add all the urls in the chanllenges app.
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # added to include challenges app urls.
+    path("challenges/", include("challenges.urls"))
 ]
