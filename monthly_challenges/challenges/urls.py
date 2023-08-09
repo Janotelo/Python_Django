c56c10d6975402e9cb8324a41fe10b698c1e3d74
Janotelo <janos.jantoc@gmail.com>
2023-08-09 11:15:43 +0800

    Added a more complext HTML codes for the main page of the website.

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views  # imported views.py
 
 urlpatterns = [
+    path("", views.index), # triggers /challenges/ PATH
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]  # List for URLs that is needed to be supported.
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
-# Create your views here.
-
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
     "february": "Walk for at least 20 minutes every day!",
@@ -20,6 +18,21 @@ monthly_challenges = {
     "december": "This is december."
 }
 
+# Create your views here.
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\"> {capitalized_month}  </a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 
 def monthly_challenge_by_number(request, month):
     # .keys is a part of a dictionary | key:value pair
@@ -30,7 +43,8 @@ def monthly_challenge_by_number(request, month):
 
     redirect_month = months[month - 1]
     # reverse will refer to the name of the PATH in the URLs
-    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    redirect_path = reverse(
+        "month-challenge", args=[redirect_month])  # /challenge/january
     return HttpResponseRedirect(redirect_path)
 
 
