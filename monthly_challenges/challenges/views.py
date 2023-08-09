1cc18aa8510fdccb4100d4217e905ba6660d27ad
Janotelo <janos.jantoc@gmail.com>
2023-08-09 15:51:47 +0800

    Added index.html | Utilized URL Tag for Dynamic URL

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -22,16 +22,11 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\"> {capitalized_month}  </a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
