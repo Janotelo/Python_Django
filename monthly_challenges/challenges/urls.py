02f9dbc26397d39a4e5423b518d718bf290b036b
Janotelo <janos.jantoc@gmail.com>
2023-08-10 16:12:50 +0800

    Utilized URL function in header.html | Inserted name parameter in the URLs

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  # imported views.py
 
 urlpatterns = [
-    path("", views.index), # triggers /challenges/ PATH
+    path("", views.index, name="index"), # triggers /challenges/ PATH
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]  # List for URLs that is needed to be supported.
