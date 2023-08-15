1e0e24fe7b86e27de1efa70750a0bfc0f69535a9
Janotelo <janos.jantoc@gmail.com>
2023-08-15 20:10:14 +0800

    Added a dummy data in the views.py

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,73 @@
+from datetime import date
+
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Janotelo",
+        "date": date(2023, 8, 15),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+            Rising majestically towards the sky, mountains stand as ancient 
+            sentinels of the Earth's grandeur. Their rugged peaks, cloaked in 
+            mist and adorned with glistening snow, reach heights that touch 
+            the very heavens. These monumental formations are a testament to 
+            the slow and patient work of geological forces, shaping landscapes 
+            over millions of years. Each mountain range tells a unique story, 
+            bearing witness to the passage of time and the resilience of 
+            nature. As the sun paints their slopes with hues of gold at dawn 
+            and casts long shadows in the embrace of twilight, mountains evoke 
+            a sense of awe and contemplation, inviting adventurers and 
+            dreamers alike to ascend their slopes and discover the world from 
+            a perspective reserved only for those who dare to climb.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.png",
+        "author": "Janotelo",
+        "date": date(2023, 8, 15),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpeg",
+        "author": "Janotelo",
+        "date": date(2023, 8, 15),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
 
 # Create your views here.
 
