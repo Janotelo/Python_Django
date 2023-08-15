8e7d1b32d2d7e0da1d08ef4be2a195ebaba3886c
Janotelo <janos.jantoc@gmail.com>
2023-08-15 13:44:47 +0800

    Inserted the views in the url paths | Added name for each path

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from . import views
+
 urlspattern = [
-    path(""),
-    path("posts"),
-    path("posts/<slug:slug>") #/posts/my-first-post {slug format}
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    # /posts/my-first-post {slug format}
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
