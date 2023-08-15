24b241b0b9e9dac4972193e2e167e8a84e753aa9
Janotelo <janos.jantoc@gmail.com>
2023-08-15 14:11:53 +0800

    Inserted render on views | Included initial setup for index.html

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
+
 
 def posts(request):
     pass
+
 
 def post_detail(request):
     pass
