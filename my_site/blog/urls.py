914fdabb599be278506264f75291a68e5b606ac8
Janotelo <janos.jantoc@gmail.com>
2023-08-15 13:53:56 +0800

    Changed urlspattern into urlpatters because this is an error

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlspattern = [
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     # /posts/my-first-post {slug format}
