931393488599c6c47c94f06e7e1c9b0a0cdf8ff5
Janotelo <janos.jantoc@gmail.com>
2023-08-15 20:43:41 +0800

    Change the list into a for-loop statement | Added a value to send in html from views.py

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -84,7 +84,9 @@ def starting_page(request):
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
