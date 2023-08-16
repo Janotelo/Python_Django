8b2659a414321bcc374800efd467c1c8387e9357
Janotelo <janos.jantoc@gmail.com>
2023-08-16 12:13:24 +0800

    Created initial model for book_outlet

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+### *This are Django Function where it converts  ###
+### python command to sql commands               ###
+### *Can look for 'django model field reference' ###
+### *Don't use reserved names                    ###
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = 
