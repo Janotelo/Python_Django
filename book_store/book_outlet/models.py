80b4a2617a0b75d25a55680f17c90d591f705fc5
Janotelo <janos.jantoc@gmail.com>
2023-08-16 15:12:01 +0800

    Return a string for data in interactive shell

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -9,3 +9,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
