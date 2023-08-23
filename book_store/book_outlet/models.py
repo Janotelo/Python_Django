7cbda7f29fab33eae9b4c4451e8aebc6a924fc8f
Janotelo <janos.jantoc@gmail.com>
2023-08-23 14:58:42 +0800

    Utilized get_absolute_url added in the models.py of the book_outlet app

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,6 +15,10 @@ class Book(models.Model):
     )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
