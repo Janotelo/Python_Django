539312896c6327904485a35ed6a80ebdb9d33e1c
Janotelo <janos.jantoc@gmail.com>
2023-08-23 14:26:46 +0800

    Added a query on views.py from models.py | Added for-loop in the index.html to get the query

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Book
 # Create your views here.
 
 
 def index(request):
-    return render(request, "book_outlet/index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
