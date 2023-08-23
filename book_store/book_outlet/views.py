08901c5aba27333f09a594b081ce9a621b6e7c95
Janotelo <janos.jantoc@gmail.com>
2023-08-23 14:40:19 +0800

    Initialized book_detail.html to get data from query | Added render function for book_details | Added it to paths

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -9,3 +9,12 @@ def index(request):
     return render(request, "book_outlet/index.html", {
         "books": books
     })
+
+def book_detail(request, id):
+    book = Book.objects.get(pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
