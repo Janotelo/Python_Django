f1b3e1c86015b87fd3925a2b36878253f948c2ac
Janotelo <janos.jantoc@gmail.com>
2023-10-26 14:56:44 +0800

    Section 100 - More config options | Added filters and more columns for showing data in admin site

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -7,5 +7,7 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     #readonly_fields = ("slug")
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
 
 admin.site.register(Book, BookAdmin)
