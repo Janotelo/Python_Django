74a12819c1180783c2dba7ef82b1bd3c636ef96f
Janotelo <janos.jantoc@gmail.com>
2023-10-26 20:17:58 +0800

    Section 108 - Adding a one-to-one Relation | Made migrations because Address entity is added.

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,10 +10,17 @@ from django.utils.text import slugify
 ### *Can look for 'django model field reference' ###
 ### *Don't use reserved names                    ###
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
