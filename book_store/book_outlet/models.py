abf7af7e21c1683ad08671799368085dfe078ee2
Janotelo <janos.jantoc@gmail.com>
2023-10-27 10:49:03 +0800

    Section 111 - Setting up many-to-many | Added models and made migrations

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,6 +10,11 @@ from django.utils.text import slugify
 ### *Can look for 'django model field reference' ###
 ### *Don't use reserved names                    ###
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -42,6 +47,7 @@ class Book(models.Model):
         Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
