2378824a7c7ce9ca375dec2ee27c5400c61d06ac
Janotelo <janos.jantoc@gmail.com>
2023-10-31 10:39:51 +0800

    Section 128 - Using Author and Tags Data | Added tags to see single post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -31,5 +31,6 @@ def post_detail(request, slug):
     #identified_post = next(post for post in all_posts if post['slug'] == slug)
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
