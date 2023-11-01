0b593f0a16ea0c2070189c0c0aa55d43f7590c06
Janotelo <janos.jantoc@gmail.com>
2023-11-01 21:22:12 +0800

    Section 147 - Saving Data with a Modelform | Altered the views.py for the modelforms | Also showed how to update an existing data instead of adding a new data.

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,22 +2,26 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
-from .models import Review
+# from .models import Review
 
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        # existing_data = Review.objects.get(pk=1) # This is for updating a data
+        form = ReviewForm(request.POST) # instance=existing_data for updating a data
 
         if form.is_valid():
+            form.save() # For Modelform
+
             #print(form.cleaned_data)
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-                )
-            review.save()
+            ##### This is for Forms #####
+            # review = Review(
+            #     user_name=form.cleaned_data['user_name'],
+            #     review_text=form.cleaned_data['review_text'],
+            #     rating=form.cleaned_data['rating']
+            #     )
+            # review.save()
             return HttpResponseRedirect("/thank-you")
         
     else:
