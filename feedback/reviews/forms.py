44c145fe12ad43008d9bd8992f24c1302d8cc3a6
Janotelo <janos.jantoc@gmail.com>
2023-11-01 20:23:19 +0800

    Section 143 - Adding More Form Controls | Added more forms and changed the statis div in html into for-loop statement

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -8,3 +8,7 @@ class ReviewForm(forms.Form):
             "required": "Your name must not be empty",
             "max_length": "Please enter a shorter name!"
         })
+    review_text = forms.CharField(
+        label="Your Feedback", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(
+        label="Your Rating", min_value=1, max_value=5)
