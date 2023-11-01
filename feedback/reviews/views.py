440e7eecdfdf89b3fd5dc477785e42bd354760d9
Janotelo <janos.jantoc@gmail.com>
2023-11-01 18:03:49 +0800

    Section 138 - Using the Django Form Class | Forms is secured unline manual validation because altering the element in developer mode inside the browser cannot be successful

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -7,17 +7,16 @@ from .forms import ReviewForm
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
+        form = ReviewForm(request.POST)
 
-        if entered_username == "" and len(entered_username) >= 100:
-            return render(request, "reviews/review.html",{
-                "has_error": True
-            })
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
-    
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+
+    form = ReviewForm()
+
     return render(request, "reviews/review.html", {
-        "has_error": False
+        "form": form
     })
     
 
