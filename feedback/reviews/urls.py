8621fced1fdbbe2a2d3924a509377dd9ed80af35
Janotelo <janos.jantoc@gmail.com>
2023-11-01 12:57:25 +0800

    Section 136 - Handling Form Submission & Exracting Data

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.review)
+    path("", views.review),
+    path("thank-you", views.thank_you)
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+    else:
+        return render(request, "reviews/review.html")
+    
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
