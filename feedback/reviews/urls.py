9514fc54c10aeec9ee0cfa927c7742270f3313a5
Janotelo <janos.jantoc@gmail.com>
2023-11-01 21:36:07 +0800

    Section 148 - Class Based Views | Changed the view function for Review into a class based view

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.review),
+    path("", views.ReviewView.as_view()),
     path("thank-you", views.thank_you)
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,35 +1,29 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
-# from .models import Review
 
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
-        # existing_data = Review.objects.get(pk=1) # This is for updating a data
-        form = ReviewForm(request.POST) # instance=existing_data for updating a data
-
-        if form.is_valid():
-            form.save() # For Modelform
-
-            #print(form.cleaned_data)
-            ##### This is for Forms #####
-            # review = Review(
-            #     user_name=form.cleaned_data['user_name'],
-            #     review_text=form.cleaned_data['review_text'],
-            #     rating=form.cleaned_data['rating']
-            #     )
-            # review.save()
-            return HttpResponseRedirect("/thank-you")
-        
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
 
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+    
+    def post(self, request):
+        form = ReviewForm(request.POST) 
+
+        if form.is_valid():
+            form.save() 
+            return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
     
 
 def thank_you(request):
