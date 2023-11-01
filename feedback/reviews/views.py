cd042e8967d1aa09d09bd157aae8f47ecb2c8290
Janotelo <janos.jantoc@gmail.com>
2023-11-01 13:13:57 +0800

    Section 137 - Manual Form Validation & the Problems with 'that' | Added if-statement in views.py and review.html for form validation

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,6 +6,11 @@ from django.shortcuts import render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
+
+        if entered_username == "" and len(entered_username) >= 100:
+            return render(request, "reviews/review.html",{
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
     else:
@@ -13,4 +18,6 @@ def review(request):
     
 
 def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html", {
+        "has_error": False
+    })
