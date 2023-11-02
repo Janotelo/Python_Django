5415cc7134176297d04cb3fc446a6ade328fe3a3
Janotelo <janos.jantoc@gmail.com>
2023-11-02 12:48:02 +0800

    Section 166 - Adding a Form with a FileFied | Added forms.py | Used django forms for validation of file upload

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+from .forms import ProfileForm
+
 # Create your views here.
 
 def store_file(file):
@@ -11,8 +13,19 @@ def store_file(file):
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        
+        if submitted_form.is_valid():
+            store_file(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+        
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
+        
