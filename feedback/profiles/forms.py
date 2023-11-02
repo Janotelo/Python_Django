1b1a742c9b3c7852bfb081776398ac02b7967040
Janotelo <janos.jantoc@gmail.com>
2023-11-02 13:10:59 +0800

    Section 168 - Using an ImageField | Change the FileField on the model and forms into an ImageField | Did not made migrations

--- feedback/profiles/forms.py
+++ feedback/profiles/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class ProfileForm(forms.Form):
-    user_image = forms.FileField()
+    user_image = forms.ImageField()
--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")
+    image = models.ImageField(upload_to="images")
