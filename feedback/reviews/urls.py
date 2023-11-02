d749bedb437a036fc9dd700ac9ba456ccd2ed927
Janotelo <janos.jantoc@gmail.com>
2023-11-02 09:54:41 +0800

    Section 157 - DetailView | Added a shorted code which is DetailView for showing a single data.

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("reviews", views.ReviewsListView.as_view()),
-    path("reviews/<int:id>", views.SingleReviewView.as_view())
+    path("reviews/<int:pk>", views.SingleReviewView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .forms import ReviewForm
 from .models import Review
@@ -51,12 +51,6 @@ class ReviewsListView(ListView):
     #     data = base_query.filter(rating__gt=4)
     #     return data
     
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context
+    model = Review # Treated as "reviews" in the html
