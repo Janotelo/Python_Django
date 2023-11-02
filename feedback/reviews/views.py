e93c114641988f0dc68e7f21335afb24ccfb423c
Janotelo <janos.jantoc@gmail.com>
2023-11-02 10:14:36 +0800

    Section 159 - FormView | Added FormView in views.py | Basically all of this class based views are done to make the code lesser

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,34 +1,44 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    # def get(self, request):
+    #     form = ReviewForm()
+
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
     
-    def post(self, request):
-        form = ReviewForm(request.POST) 
+    ##### Automatically handled by Django #####
+    # def post(self, request):
+    #     form = ReviewForm(request.POST) 
 
-        if form.is_valid():
-            form.save() 
-            return HttpResponseRedirect("/thank-you")
+    #     if form.is_valid():
+    #         form.save() 
+    #         return HttpResponseRedirect("/thank-you")
         
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
     
 
 class ThankYouView(TemplateView):
