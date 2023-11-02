b8107d99655978607e5fd2f44b8f038226dc2d17
Janotelo <janos.jantoc@gmail.com>
2023-11-02 12:29:13 +0800

    Section 164 - Making the File Upload Work

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,4 +10,5 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        print(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
