8a49c6dcc9a5ecdc7187edf908cdb4a81f3c9bf1
Janotelo <janos.jantoc@gmail.com>
2023-11-02 13:16:32 +0800

    Section 169 - Using a CreateView | Changed the conventional view into a Class-based view CreateView | More compact than the old one

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+##### Alternative for the CreateView | Old and Longer #####
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
         
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
         
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
         
