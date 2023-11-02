09ec83447738156e14d89ab12dd1161a571c2139
Janotelo <janos.jantoc@gmail.com>
2023-11-02 13:27:22 +0800

    Section 170 - Working with the File Field | Image is still not fetch in the server

--- feedback/profiles/urls.py
+++ feedback/profiles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view())
+    path("", views.CreateProfileView.as_view()),
+    path("list", views.ProfilesView.as_view())
 ]
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -13,6 +14,14 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = "__all__"
     success_url = "/profiles"
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
+
+
 
 ##### Alternative for the CreateView | Old and Longer #####
 # class CreateProfileView(View):
