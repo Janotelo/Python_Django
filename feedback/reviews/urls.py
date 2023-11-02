010fea9ec9314bc7dcc61d5a9f7901562189815f
Janotelo <janos.jantoc@gmail.com>
2023-11-02 09:26:37 +0800

    Section 154 - Using the TemplateView | Added new template and TemplateView to show a list of reviews

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.ThankYouView.as_view())
+    path("thank-you", views.ThankYouView.as_view()),
+    path("reviews", views.ReviewsListView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -34,4 +35,14 @@ class ThankYouView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["message"] = "This works!"
+        return context
+    
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
         return context
