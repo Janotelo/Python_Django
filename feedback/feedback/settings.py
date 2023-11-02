bd672c4c6e09a7f70ed01ca8c1d77e20e70ea748
Janotelo <janos.jantoc@gmail.com>
2023-11-02 11:15:03 +0800

    Section 163 - Starting Setup | Test commit to fix profiles unkown by server

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles',
     'reviews', #for template to be found by django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
