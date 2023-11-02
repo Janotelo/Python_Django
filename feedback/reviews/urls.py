be62aa94a707dfab5f8af0eae705645f95b65e38
Janotelo <janos.jantoc@gmail.com>
2023-11-02 09:34:59 +0800

    Section 155 - Showin a Detail Template | Added a SingleReviewView in the views.py | Added a new html as a template for the view

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
-    path("reviews", views.ReviewsListView.as_view())
+    path("reviews", views.ReviewsListView.as_view()),
+    path("reviews/<int:id>", views.SingleReviewView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -46,3 +46,13 @@ class ReviewsListView(TemplateView):
         reviews = Review.objects.all()
         context["reviews"] = reviews
         return context
+    
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
+        return context
