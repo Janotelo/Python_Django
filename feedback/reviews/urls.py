f6ea69122a0e6856fda83da314de03c50912edec
Janotelo <janos.jantoc@gmail.com>
2023-11-02 09:18:45 +0800

    Section 153 - TemplateView | Addded TemplateView in views.py named ThankYouView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you)
+    path("thank-you", views.ThankYouView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -26,5 +28,10 @@ class ReviewView(View):
         })
     
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
