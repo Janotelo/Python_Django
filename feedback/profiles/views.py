c305fd2b79ba7aba2eb325473854c9d08a558766
Janotelo <janos.jantoc@gmail.com>
2023-11-02 13:00:31 +0800

    Section 167 - Using Models for File Storage | Added models for image | The database just holds the image path not the image file | Made migrations for the model

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -3,13 +3,10 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
@@ -22,7 +19,8 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
         
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
         
         return render(request, "profiles/create_profile.html", {
