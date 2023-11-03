b081966653eed2c6a215501556384b2793a8ee5b
Janotelo <janos.jantoc@gmail.com>
2023-11-03 16:41:49 +0800

    Section 194 - Outputting Comments | Showing comments of a post without syles

--- course_project_section_14/my_site/blog/views.py
+++ course_project_section_14/my_site/blog/views.py
@@ -37,7 +37,8 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -55,6 +56,7 @@ class SinglePostView(View):
         context = {
             "post": post,
             "post_tags": post.tags.all(),
-            "comment_form": comment_form
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/post-detail.html", context)
