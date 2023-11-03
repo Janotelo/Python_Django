aad966e4a952b7745210c35a0563cdfb8559afec
Janotelo <janos.jantoc@gmail.com>
2023-11-03 13:56:07 +0800

    Section 190 - Adding a Comment Form | Added forms for the comment as ModelForm

--- course_project_section_14/my_site/blog/views.py
+++ course_project_section_14/my_site/blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -34,5 +35,5 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
         return context
-    
