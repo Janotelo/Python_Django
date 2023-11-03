41e2d1b53fcfc1c5441504180b35eae7f839f6d9
Janotelo <janos.jantoc@gmail.com>
2023-11-03 13:34:13 +0800

    Section 188 - Converting Views to Class Based | all of the functions are converted to Class Based Views as a practice | You can retain the functions if you want to

--- course_project_section_14/my_site/blog/urls.py
+++ course_project_section_14/my_site/blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    # /posts/my-first-post {slug format}
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+          name="post-detail-page") # /posts/my-first-post
 ]
--- course_project_section_14/my_site/blog/views.py
+++ course_project_section_14/my_site/blog/views.py
@@ -1,36 +1,38 @@
-#from datetime import date
-
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-#def get_date(post):
-    #return post['date']
-
 # Create your views here.
 
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"] # Can add an entry
+    context_object_name = "posts" # Provided to the template
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3] #Descending Date
-    #sorted_posts = sorted(all_posts, key=get_date)
-    #latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
-
-
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
-def post_detail(request, slug):
-    # next function simply find the next element for the condition
-    #identified_post = next(post for post in all_posts if post['slug'] == slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"] # Can add an entry
+    context_object_name = "all_posts" # Provided to the template
+
+
+##### Support Slug and Automatically raise 404 error #####
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+    
