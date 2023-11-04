4b814a7b07a39d5926a448d6cc34ec41339242ae
Janotelo <janos.jantoc@gmail.com>
2023-11-04 14:06:04 +0800

    Section 200 - Finishing the Read Later Feature | Finalizing the function of the read later

--- course_project_section_14/my_site/blog/views.py
+++ course_project_section_14/my_site/blog/views.py
@@ -32,13 +32,24 @@ class AllPostsView(ListView):
 
 ##### Support Slug and Automatically raise 404 error #####
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -57,7 +68,8 @@ class SinglePostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
     
@@ -87,6 +99,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+            
+        request.session["stored_posts"] = stored_posts
 
         return HttpResponseRedirect("/")
